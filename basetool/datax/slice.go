25845a27c10227bfe7a64c241cdd78202fc9c8f7
wwqdrh <huiloademail@163.com>
2023-01-02 12:46:45 +0000

    safe

---
+++ basetool/datax/slice.go
@@ -0,0 +1,69 @@
+package datax
+
+import (
+	"errors"
+	"sync/atomic"
+)
+
+type SafeSlice[T any] struct {
+	head   *sliceNode[T]
+	tail   *sliceNode[T]
+	length int64
+	ch     chan T // 通道用来接收添加数据
+}
+
+type sliceNode[T any] struct {
+	Val  T
+	Next *sliceNode[T]
+}
+
+func (SafeSlice[T]) NewSafeSlice(maxLength int) *SafeSlice[T] {
+	s := &SafeSlice[T]{
+		head:   nil,
+		tail:   nil,
+		length: 0,
+		ch:     make(chan T, maxLength),
+	}
+	go func() {
+		for item := range s.ch {
+			cur := &sliceNode[T]{
+				Val: item,
+			}
+			if atomic.LoadInt64(&s.length) == 0 {
+				s.head = cur
+				s.tail = cur
+			} else {
+				s.tail.Next = cur
+				s.tail = cur
+			}
+			atomic.AddInt64(&s.length, 1)
+		}
+	}()
+
+	return s
+}
+
+// 基于channel
+func (s *SafeSlice[T]) Add(value T) {
+	s.ch <- value
+}
+
+func (s *SafeSlice[T]) Get() ([]T, error) {
+	length := atomic.LoadInt64(&s.length)
+	if length == 0 {
+		return nil, errors.New("数据为空")
+	}
+
+	data := []T{}
+
+	var cur *sliceNode[T]
+	for i := 0; i < int(length); i++ {
+		if i == 0 {
+			cur = s.head
+		} else {
+			cur = cur.Next
+		}
+		data = append(data, cur.Val)
+	}
+	return data, nil
+}
---
+++ basetool/datax/slice_test.go
@@ -0,0 +1,38 @@
+package datax
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestSafeStringSlice(t *testing.T) {
+	slice := (SafeSlice[string]{}).NewSafeSlice(10)
+
+	go func() {
+		slice.Add("1")
+	}()
+	go func() {
+		slice.Add("2")
+	}()
+	go func() {
+		slice.Add("3")
+	}()
+	go func() {
+		data, err := slice.Get()
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(data)
+	}()
+	go func() {
+		data, err := slice.Get()
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(data)
+	}()
+	time.Sleep(3 * time.Second)
+}
