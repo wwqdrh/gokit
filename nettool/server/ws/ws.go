ebf440fda79bf2d70ccaf2c864a0dc7e68690037
wwqdrh <huiloademail@163.com>
2023-12-05 21:48:09 +0800

    ws

--- nettool/server/ws/ws.go
+++ nettool/server/ws/ws.go
@@ -1,74 +1,501 @@
-package ws
+package engine
 
 import (
-	"encoding/json"
+	"bytes"
+	"context"
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"net"
 	"net/http"
+	"net/url"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/wwqdrh/gokit/logger"
 )
 
-var Upgrader = websocket.Upgrader{
+// if (window["WebSocket"]) {
+// 	conn = new WebSocket("ws://" + document.location.host + "/ws");
+// 	conn.onclose = function (evt) {
+// 		var item = document.createElement("div");
+// 		item.innerHTML = "<b>Connection closed.</b>";
+// 		document.body.append(item);
+// 	};
+// 	conn.onmessage = function (evt) {
+// 		var messages = evt.data.split('\n');
+// 		for (var i = 0; i < messages.length; i++) {
+// 			var item = document.createElement("div");
+// 			item.innerText = messages[i];
+// 			document.body.append(item);
+// 		}
+// 	};
+// } else {
+// 	var item = document.createElement("div");
+// 	item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
+// 	document.body.append(item);
+// }
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
-	WriteBufferSize: 1024 * 1024 * 10,
+	WriteBufferSize: 1024,
+	// 解决跨域问题
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
 
-var conns = make(map[uint64]*websocket.Conn, 100)
+// Hub maintains the set of active clients and broadcasts messages to the
+// clients.
+type WSHub struct {
+	// Registered clients.
+	clients map[*Client]bool
 
-func init() {
-	checkConn()
+	// Inbound messages from the clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
 }
 
-// 放置ws连接
-func Put(userId uint64, conn *websocket.Conn) {
-	existConn := conns[userId]
-	if existConn != nil {
-		Delete(userId)
+func NewHub() *WSHub {
+	return &WSHub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
 	}
-
-	conn.SetCloseHandler(func(code int, text string) error {
-		Delete(userId)
-		return nil
-	})
-	conns[userId] = conn
 }
 
-func checkConn() {
-	heartbeat := time.Duration(60) * time.Second
-	tick := time.NewTicker(heartbeat)
-	go func() {
-		for range tick.C {
-			// 遍历所有连接，ping失败的则删除掉
-			for uid, conn := range conns {
-				err := conn.WriteControl(websocket.PingMessage, []byte("ping"), time.Now().Add(heartbeat/2))
-				if err != nil {
-					Delete(uid)
-					return
+func (h *WSHub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
 				}
 			}
 		}
+	}
+}
+
+var hubPool = sync.Map{}
+
+// Client is a middleman between the websocket connection and the hub.
+type Client struct {
+	Hub *WSHub
+
+	// The websocket connection.
+	conn *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+
+	recv chan []byte
+}
+
+func NewWSClient(rw http.ResponseWriter, req *http.Request, header http.Header) (*Client, error) {
+	conn, err := upgrader.Upgrade(rw, req, header)
+	if err != nil {
+		return nil, err
+	}
+	client := &Client{conn: conn, send: make(chan []byte, 256), recv: make(chan []byte, 256)}
+	if client.Hub != nil {
+		client.Hub.register <- client
+	}
+
+	// Allow collection of memory referenced by the caller by doing all work in
+	// new goroutines.
+	go client.writePump()
+	go client.readPump()
+	return client, nil
+}
+
+func NewWSClientWithConn(conn *websocket.Conn) (*Client, error) {
+	client := &Client{conn: conn, send: make(chan []byte, 256), recv: make(chan []byte, 256)}
+	if client.Hub != nil {
+		client.Hub.register <- client
+	}
+	go client.writePump()
+	go client.readPump()
+	return client, nil
+}
+
+func (c *Client) GetConn() *websocket.Conn {
+	return c.conn
+}
+
+// 加入一个hub，在这里发送消息，其他的所有的连接也都会收到
+func (c *Client) Join(key string) error {
+	var hub *WSHub
+	if val, ok := hubPool.Load(key); !ok {
+		hub = NewHub()
+		go hub.Run()
+		hubPool.Store(key, hub)
+	} else {
+		var ok bool
+		hub, ok = val.(*WSHub)
+		if !ok {
+			return errors.New("type is err")
+		}
+	}
+
+	c.Hub = hub
+	hub.register <- c
+	return nil
+}
+
+func (c *Client) Read(ctx context.Context) (string, error) {
+	select {
+	case msg := <-c.recv:
+		return string(msg), nil
+	case <-ctx.Done():
+		return "", errors.New("no value")
+	}
+}
+
+func (c *Client) Write(ctx context.Context, msg string) error {
+	select {
+	case c.send <- []byte(msg):
+		return nil
+	case <-ctx.Done():
+		return errors.New("time out")
+	}
+}
+
+func (c *Client) Close() {
+	close(c.send)
+}
+
+func (c *Client) Broadcast(ctx context.Context, msg []byte) error {
+	if c.Hub != nil {
+		c.Hub.broadcast <- msg
+		return nil
+	} else {
+		return errors.New("the hub is nil")
+	}
+}
+
+// readPump pumps messages from the websocket connection to the hub.
+//
+// The application runs readPump in a per-connection goroutine. The application
+// ensures that there is at most one reader on a connection by executing all
+// reads from this goroutine.
+func (c *Client) readPump() {
+	defer func() {
+		if c.Hub != nil {
+			c.Hub.unregister <- c
+		}
+		c.conn.Close()
 	}()
-}
-
-// 删除ws连接
-func Delete(userid uint64) {
-	fmt.Println("移除websocket连接：uid = ", userid)
-	conn := conns[userid]
-	if conn != nil {
-		conn.Close()
-		delete(conns, userid)
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.recv <- message
 	}
 }
 
-// 对指定用户发送消息
-func SendMsg(userId uint64, msg *Msg) {
-	conn := conns[userId]
-	if conn != nil {
-		bytes, _ := json.Marshal(msg)
-		conn.WriteMessage(websocket.TextMessage, bytes)
+// writePump pumps messages from the hub to the websocket connection.
+//
+// A goroutine running writePump is started for each connection. The
+// application ensures that there is at most one writer to a connection by
+// executing all writes from this goroutine.
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message.
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
 	}
+}
+
+var (
+	// DefaultDialer is a dialer with all fields set to the default zero values.
+	DefaultDialer = websocket.DefaultDialer
+)
+
+// WebsocketProxy is an HTTP Handler that takes an incoming WebSocket
+// connection and proxies it to another server.
+type WebsocketProxy struct {
+	// Director, if non-nil, is a function that may copy additional request
+	// headers from the incoming WebSocket connection into the output headers
+	// which will be forwarded to another server.
+	Director func(incoming *http.Request, out http.Header)
+
+	// Backend returns the backend URL which the proxy uses to reverse proxy
+	// the incoming WebSocket connection. Request is the initial incoming and
+	// unmodified request.
+	Backend func(*http.Request) *url.URL
+
+	// Upgrader specifies the parameters for upgrading a incoming HTTP
+	// connection to a WebSocket connection. If nil, DefaultUpgrader is used.
+	Upgrader *websocket.Upgrader
+
+	//  Dialer contains options for connecting to the backend WebSocket server.
+	//  If nil, DefaultDialer is used.
+	Dialer *websocket.Dialer
+}
+
+// ProxyHandler returns a new http.Handler interface that reverse proxies the
+// request to the given target.
+func ProxyHandler(target *url.URL) http.Handler { return NewProxy(target) }
+
+// NewProxy returns a new Websocket reverse proxy that rewrites the
+// URL's to the scheme, host and base path provider in target.
+func NewProxy(target *url.URL) *WebsocketProxy {
+	backend := func(r *http.Request) *url.URL {
+		// Shallow copy
+		u := *target
+		u.Fragment = r.URL.Fragment
+		u.Path = r.URL.Path
+		u.RawQuery = r.URL.RawQuery
+		return &u
+	}
+	return &WebsocketProxy{Backend: backend, Upgrader: &upgrader}
+}
+
+// ServeHTTP implements the http.Handler that proxies WebSocket connections.
+func (w *WebsocketProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if w.Backend == nil {
+		log.Println("websocketproxy: backend function is not defined")
+		http.Error(rw, "internal server error (code: 1)", http.StatusInternalServerError)
+		return
+	}
+
+	backendURL := w.Backend(req)
+	if backendURL == nil {
+		log.Println("websocketproxy: backend URL is nil")
+		http.Error(rw, "internal server error (code: 2)", http.StatusInternalServerError)
+		return
+	}
+
+	dialer := w.Dialer
+	if w.Dialer == nil {
+		dialer = DefaultDialer
+	}
+
+	// Pass headers from the incoming request to the dialer to forward them to
+	// the final destinations.
+	requestHeader := http.Header{}
+	if origin := req.Header.Get("Origin"); origin != "" {
+		requestHeader.Add("Origin", origin)
+	}
+	for _, prot := range req.Header[http.CanonicalHeaderKey("Sec-WebSocket-Protocol")] {
+		requestHeader.Add("Sec-WebSocket-Protocol", prot)
+	}
+	for _, cookie := range req.Header[http.CanonicalHeaderKey("Cookie")] {
+		requestHeader.Add("Cookie", cookie)
+	}
+	if req.Host != "" {
+		requestHeader.Set("Host", req.Host)
+	}
+
+	// Pass X-Forwarded-For headers too, code below is a part of
+	// httputil.ReverseProxy. See http://en.wikipedia.org/wiki/X-Forwarded-For
+	// for more information
+	// TODO: use RFC7239 http://tools.ietf.org/html/rfc7239
+	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+		// If we aren't the first proxy retain prior
+		// X-Forwarded-For information as a comma+space
+		// separated list and fold multiple headers into one.
+		if prior, ok := req.Header["X-Forwarded-For"]; ok {
+			clientIP = strings.Join(prior, ", ") + ", " + clientIP
+		}
+		requestHeader.Set("X-Forwarded-For", clientIP)
+	}
+
+	// Set the originating protocol of the incoming HTTP request. The SSL might
+	// be terminated on our site and because we doing proxy adding this would
+	// be helpful for applications on the backend.
+	requestHeader.Set("X-Forwarded-Proto", "http")
+	if req.TLS != nil {
+		requestHeader.Set("X-Forwarded-Proto", "https")
+	}
+
+	// Enable the director to copy any additional headers it desires for
+	// forwarding to the remote server.
+	if w.Director != nil {
+		w.Director(req, requestHeader)
+	}
+
+	// Connect to the backend URL, also pass the headers we get from the requst
+	// together with the Forwarded headers we prepared above.
+	// TODO: support multiplexing on the same backend connection instead of
+	// opening a new TCP connection time for each request. This should be
+	// optional:
+	// http://tools.ietf.org/html/draft-ietf-hybi-websocket-multiplexing-01
+	connBackend, resp, err := dialer.Dial(backendURL.String(), requestHeader)
+	if err != nil {
+		log.Printf("websocketproxy: couldn't dial to remote backend url %s", err)
+		if resp != nil {
+			// If the WebSocket handshake fails, ErrBadHandshake is returned
+			// along with a non-nil *http.Response so that callers can handle
+			// redirects, authentication, etcetera.
+			if err := copyResponse(rw, resp); err != nil {
+				log.Printf("websocketproxy: couldn't write response after failed remote backend handshake: %s", err)
+			}
+		} else {
+			http.Error(rw, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+		}
+		return
+	}
+	backend, err := NewWSClientWithConn(connBackend)
+	if err != nil {
+		log.Printf("websocketproxy: couldn't upgrade %s", err)
+		return
+	}
+	defer backend.Close()
+
+	// Only pass those headers to the upgrader.
+	upgradeHeader := http.Header{}
+	if hdr := resp.Header.Get("Sec-Websocket-Protocol"); hdr != "" {
+		upgradeHeader.Set("Sec-Websocket-Protocol", hdr)
+	}
+	if hdr := resp.Header.Get("Set-Cookie"); hdr != "" {
+		upgradeHeader.Set("Set-Cookie", hdr)
+	}
+
+	// Now upgrade the existing incoming request to a WebSocket connection.
+	// Also pass the header that we gathered from the Dial handshake.
+
+	client, err := NewWSClient(rw, req, upgradeHeader)
+	if err != nil {
+		log.Printf("websocketproxy: couldn't upgrade %s", err)
+		return
+	}
+	defer client.Close()
+
+	errClient := make(chan error, 1)
+	errBackend := make(chan error, 1)
+	replicateWebsocketConn := func(dst, src *Client, errc chan error) {
+		for {
+			msg, err := src.Read(context.TODO())
+			if err != nil {
+				m := websocket.FormatCloseMessage(websocket.CloseNormalClosure, fmt.Sprintf("%v", err))
+				if e, ok := err.(*websocket.CloseError); ok {
+					if e.Code != websocket.CloseNoStatusReceived {
+						m = websocket.FormatCloseMessage(e.Code, e.Text)
+					}
+				}
+				errc <- err
+				// dst.WriteMessage(websocket.CloseMessage, m)
+				logger.Get("edge").Warn(string(m))
+				break
+			}
+			err = dst.Write(context.TODO(), msg)
+			if err != nil {
+				errc <- err
+				break
+			}
+		}
+	}
+
+	go replicateWebsocketConn(client, backend, errClient)
+	go replicateWebsocketConn(backend, client, errBackend)
+
+	var message string
+	select {
+	case err = <-errClient:
+		message = "websocketproxy: Error when copying from backend to client: %v"
+	case err = <-errBackend:
+		message = "websocketproxy: Error when copying from client to backend: %v"
+
+	}
+	if e, ok := err.(*websocket.CloseError); !ok || e.Code == websocket.CloseAbnormalClosure {
+		log.Printf(message, err)
+	}
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func copyResponse(rw http.ResponseWriter, resp *http.Response) error {
+	copyHeader(rw.Header(), resp.Header)
+	rw.WriteHeader(resp.StatusCode)
+	defer resp.Body.Close()
+
+	_, err := io.Copy(rw, resp.Body)
+	return err
 }
